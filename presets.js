c2d22db3283440d62e2d7b42908c07d22b02ca04
ckangwen <2437037469@qq.com>
2022-10-22 09:04:14 +0800

    feat: add monorepo starter

--- presets.js
+++ presets.js
@@ -3,4 +3,5 @@ export default [
   { title: "TypeScript Koa Starter", value: "ckangwen/typescript-koa-template" },
   { title: "Vue Admin Template(ElementUI + Vue2)", value: "ckangwen/vue-ele-admin-v2-template" },
   { title: "Vue Admin Template(ElementPlus + Vue3)", value: "ckangwen/vue-ele-admin-v3-template" },
+  { title: "TypeScript Monorepo Starter", value: "ckangwen/ts-monorepo-starter" },
 ];
