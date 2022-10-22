6adf5bfe9971b5dd4614b37905bd453f8f3ac174
ckangwen <2437037469@qq.com>
2022-10-22 09:44:15 +0800

    style: lint

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ import path from "path";
 import prompts from "prompts";
 import { red, cyan, green } from "kolorist";
 import minimist from "minimist";
-import degit from "degit"
+import degit from "degit";
 import presets from "./presets";
-import { spawnSync } from "child_process"
+import { writePackageJson, gitInit } from "./utils";
 
+// eslint-disable-next-line max-statements
 async function init() {
   const cwd = process.cwd();
-
   const argv = minimist(process.argv.slice(2), {});
 
   let targetDir = argv._[0];
   const defaultProjectName = !targetDir ? "my-project" : targetDir;
-
   let result = {};
 
   try {
@@ -45,8 +44,7 @@ async function init() {
     process.exit(1);
   }
 
-  const userRepo = result.preset
-
+  const userRepo = result.preset;
   let dist = path.join(cwd, targetDir);
 
   if (fs.existsSync(dist)) {
@@ -63,56 +61,11 @@ async function init() {
     await emitter.clone(dist);
     console.log(`${green("✔ cloned")} ${cyan(userRepo)} to ${cyan(targetDir)}`);
 
-    const ls = spawnSync("git", ["init"], { encoding: "utf-8" });
-    if (!ls || !ls.stdout) {
-      console.log(`${red("✖")} git init failed!`);
-    } else {
-      console.log(ls.stdout);
-    }
-
     writePackageJson(dist, "name", targetDir);
+
+    gitInit();
   } catch (e) {
     console.error(e);
-  }
-}
-
-const readPackageJson = (dir) => {
-  const packageJsonPath = path.join(dir, "package.json");
-
-  const exists = fs.existsSync(packageJsonPath);
-
-  if (!exists) {
-    throw new Error(`package.json not found at: ${packageJsonPath}`);
-  }
-
-  const packageJsonString = fs.readFileSync(packageJsonPath, "utf8");
-
-  try {
-    return JSON.parse(packageJsonString);
-  } catch (error) {
-    throw new Error(`Cannot parse package.json: ${error.message}`);
-  }
-}
-
-const writePackageJson = (dir, key, value) => {
-  try {
-    const packageJsonPath = path.join(dir, "package.json");
-    const data = readPackageJson(dir);
-    data[key] = value;
-
-    fs.writeFileSync(
-      packageJsonPath,
-      JSON.stringify(data, null, 2),
-      {
-        encoding: "utf-8"
-      }
-    );
-  } catch (e) {
-    if (e.message) {
-      console.log(`${red(e.message)}`);
-    } else {
-      console.error(e)
-    }
   }
 }
 
---
+++ utils.js
@@ -0,0 +1,53 @@
+import fs from "fs";
+import path from "path";
+import { spawnSync } from "child_process";
+import { red } from "kolorist";
+
+export const readPackageJson = (dir) => {
+  const packageJsonPath = path.join(dir, "package.json");
+
+  const exists = fs.existsSync(packageJsonPath);
+
+  if (!exists) {
+    throw new Error(`package.json not found at: ${packageJsonPath}`);
+  }
+
+  const packageJsonString = fs.readFileSync(packageJsonPath, "utf8");
+
+  try {
+    return JSON.parse(packageJsonString);
+  } catch (error) {
+    throw new Error(`Cannot parse package.json: ${error.message}`);
+  }
+};
+
+export const writePackageJson = (dir, key, value) => {
+  try {
+    const packageJsonPath = path.join(dir, "package.json");
+    const data = readPackageJson(dir);
+    data[key] = value;
+
+    fs.writeFileSync(
+      packageJsonPath,
+      JSON.stringify(data, null, 2),
+      {
+        encoding: "utf-8",
+      },
+    );
+  } catch (e) {
+    if (e.message) {
+      console.log(`${red(e.message)}`);
+    } else {
+      console.error(e);
+    }
+  }
+};
+
+export const gitInit = () => {
+  const ls = spawnSync("git", ["init"], { encoding: "utf-8" });
+  if (!ls || !ls.stdout) {
+    console.log(`${red("✖")} git init failed!`);
+  } else {
+    console.log(ls.stdout);
+  }
+};
